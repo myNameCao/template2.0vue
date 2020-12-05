f6ff9d25ba0b7d2a1b20c9167f8dac566a198eb0
caohefei <hefei.cao@xiaoyangedu.com>
2020-12-05 13:55:51 +0800

    fix

---
+++ pc/template/src/views/common/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    欢迎您。。。。。
+    <div class="run"></div>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="less" scpoed>
+  .home {
+    width: 750px;
+    box-sizing: border-box;
+    .run {
+      width: 375px;
+      height: 200px;
+    }
+  }
+</style>
