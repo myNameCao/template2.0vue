ee52732593452a8312c4606656a4d3837d1f38ec
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-21 11:10:31 +0800

    ci: commit-msg

--- default/commit-msg.js
+++ default/commit-msg.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 const childProcess = require('child_process')
+const chalk = require('chalk')
 const fs = require('fs')
-
-const email = childProcess
-  .execSync('git config user.email')
-  .toString()
-  .trim()
+const email = childProcess.execSync('git config user.email').toString().trim()
 const msg = fs.readFileSync(process.argv[2], 'utf-8').trim() // 索引 2 对应的 commit 消息文件
-const commitRE = /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,100}/
+const commitRE =
+  /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,100}/
 if (!commitRE.test(msg)) {
-  console.error('不合法的 commit 消息格式，请使用正确的提交格式：')
-  console.error('feat: add "comments" option')
+  /* eslint-disable */
   console.error(
-    '详情请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md。'
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      'invalid commit message format.'
+    )}\n\n` +
+      chalk.red(
+        '  Proper commit message format is required for automated changelog generation. Examples:\n\n'
+      ) +
+      `    ${chalk.green('feat(compiler): add "comments" option')}\n` +
+      `    ${chalk.green(
+        'fix(v-model): handle events on blur (close #28)'
+      )}\n\n` +
+      chalk.red(
+        '详情请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md'
+      )
   )
   process.exit(1)
 }
